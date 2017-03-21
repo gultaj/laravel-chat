d976ac295b78bf92314bda01a3bd521c6fc857af
gultaj <yaschick.mail@gmail.com>
2017-03-22 01:12:33 +0300

    delete dusk, configure acceptance

--- tests/_support/Helper/Acceptance.php
+++ tests/_support/Helper/Acceptance.php
@@ -6,5 +6,23 @@ namespace Helper;
 
 class Acceptance extends \Codeception\Module
 {
+    public function _beforeSuite($settings = [])
+    {
+        \Illuminate\Support\Facades\Artisan::call('migrate');
+        copy(base_path('.env'), base_path('.env-backup'));
+        copy(base_path('.env.testing'), base_path('.env'));
+    }
 
+    public function _afterSuite()
+    {
+        copy(base_path('.env-backup'), base_path('.env'));
+        unlink(base_path('.env-backup'));
+        \Illuminate\Support\Facades\Artisan::call('migrate:reset');
+    }
+
+    // public function _failed(\Codeception\TestInterface $test, $fail)
+    // {
+    //     $this->_afterSuite();
+    // }
+    
 }
