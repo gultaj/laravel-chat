60843b773159a30527e51c9c07525c28c493b9f2
gultaj <yaschick.mail@gmail.com>
2017-03-23 00:20:21 +0300

    delete dusk provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,8 +25,5 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if ($this->app->environment('local', 'testing')) {
-            $this->app->register(DuskServiceProvider::class);
-        }
     }
 }
