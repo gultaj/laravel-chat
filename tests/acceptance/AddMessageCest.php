c4f5585efe21970cf2d4453ebfdc24f6475bda87
gultaj <yaschick.mail@gmail.com>
2017-04-01 01:03:01 +0300

    see loaded messaged

--- tests/acceptance/AddMessageCest.php
+++ tests/acceptance/AddMessageCest.php
@@ -10,6 +10,7 @@ class AddMessageCest
         $message = factory(App\Message::class)->make();
 
         $I->wantTo('see new message after creation');
+        // $user = $I->have('App\User');
         $I->loginAs($user);
         $I->amOnPage('/home');
         $I->fillField('message', $message->title);
@@ -17,5 +18,25 @@ class AddMessageCest
         $I->waitForText($message->title, 15);
         $I->seeInDatabase('messages', ['title' => $message->title]);
         $I->see($message->title);
+    }
+
+    public function seeLoadedMessages(AcceptanceTester $I)
+    {
+        $user1 = factory(App\User::class)->create();
+        $user2 = factory(App\User::class)->create();
+        $message1 = factory(App\Message::class)->make();
+        $message2 = factory(App\Message::class)->make();
+        $user1->messages()->create($message1->toArray());
+        $user2->messages()->create($message2->toArray());
+
+        $I->wantTo('see loaded messages');
+        $I->loginAs($user1);
+        $I->amOnPage('/home');
+        $I->waitForText($message1->title, 10);
+        $I->waitForText($message2->title, 10);
+        $I->seeInDatabase('messages', ['title' => $message1->title]);
+        $I->seeInDatabase('messages', ['title' => $message2->title]);
+        $I->see($message1->title);
+        $I->see($message2->title);
     }
 }
